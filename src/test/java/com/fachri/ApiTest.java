a2be3daacf0b7b95061e232684de656e87da11b2
fachri <fachriaje21@gmail.com>
2025-08-28 14:55:50 +0700

    update feature

--- src/test/java/com/fachri/ApiTest.java
+++ src/test/java/com/fachri/ApiTest.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         glue = {"com.fachri"},
-        features = {"src/test/resources/api.feature"},
+        features = {"src/test/resources"},
         plugin = {"pretty","html:reports/api/api-report.html", "json:reports/api/api-report.json"} //{"pretty"} // menampilkan test per log
 )
 public class ApiTest {
